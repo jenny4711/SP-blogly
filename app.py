7e4302ff645273f985cbf84083fcec89ba91b794
jiylee <jjenny4711@gmail.com>
2022-12-16 12:52:07 -0500

    d

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///blogly22')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgres:///blogly22')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
