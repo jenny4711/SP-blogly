57c08170c0cad796e796f04a537da189c3190acf
jiylee <jjenny4711@gmail.com>
2022-12-20 22:37:30 -0500

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db, connect_db, Users,Post,get_name,Tag,PostTag
 from seed import setup
 import os
 import re
-
+app = Flask(__name__)
 
 
 uri = os.environ.get("DATABASE_URL","postgres://blogly22")  
@@ -12,7 +12,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql:///",1 )
     
 
-app = Flask(__name__)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get("DATABASE_URL","postgresql:///blogly22")
 app.config['SQLALCHEMY_DATABASE_URI'] =uri
 print(os.environ)
